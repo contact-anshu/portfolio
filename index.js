ded098e5b45d9b379c565d60f222a63572d32a34
contact-anshu <135035538+contact-anshu@users.noreply.github.com>
2023-08-07 16:54:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function bigImg(x) {
+    x.style.height = "210px";
+    x.style.width = "210px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
+  }
